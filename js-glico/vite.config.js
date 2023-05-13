3165642d2ad0b18a870e1d0077fab7928b2fb5c1
armanriazi <arman2riazi@gmail.com>
2023-05-13 10:42:21 +0000

    first to dev

---
+++ js-glico/vite.config.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+import { defineConfig } from "vite";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+  
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+});
