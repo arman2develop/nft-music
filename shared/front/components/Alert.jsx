db84646e7152cb6c50fba5daa0700bcd71a63196
armanriazi <arman2riazi@gmail.com>
2023-05-21 19:11:32 +0000

    stable-ok-before-start unqt

--- shared/front/components/Alert.jsx
+++
@@ -1,3 +0,0 @@
-export default function Alert({ children }) {
-  return <div>{children}</div>;
-}
--- shared/front/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-  padding: 1em;
-  background-color: red;
-`;
-
-export default Button;
