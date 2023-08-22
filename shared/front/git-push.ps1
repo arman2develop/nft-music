d3831e9bdbd8b9b4bbc1556212da24f5c0616ae7
armanriazi <armanriazi@hotmail.com>
2023-08-22 00:02:39 +0000

    s-win

--- shared/front/git-push.ps1
+++ shared/front/git-push.ps1
@@ -1,17 +1,5 @@
-git status
-echo "Enter your message"
-read message
-#echo 'Enter the name of the branch:'
-#read branch
+$message="commit ps0"
 git add .
-git commit -m "${message}"
-
-#if [ -n "$(git status - porcelain)" ];
-#then
-# echo "IT IS CLEAN"
-#else
-#
-
- echo "Pushing data to remote server!!!"
-
- git push -u origin main
+git commit -m $($message)
+write-host "Pushing data to remote server!!!"
+git push -u origin main
